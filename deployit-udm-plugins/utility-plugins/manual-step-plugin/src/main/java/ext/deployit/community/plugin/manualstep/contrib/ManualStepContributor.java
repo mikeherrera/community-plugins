e2d8fda4baea6ab99cbe00fccef454d4218e23bb
Mike Herrera <maherrera@tribune.com>
2014-08-25 16:34:03 -0500

    Extended triggerManualSteps method to account for manual steps attached to the container of every deployed.

--- deployit-udm-plugins/utility-plugins/manual-step-plugin/src/main/java/ext/deployit/community/plugin/manualstep/contrib/ManualStepContributor.java
+++ deployit-udm-plugins/utility-plugins/manual-step-plugin/src/main/java/ext/deployit/community/plugin/manualstep/contrib/ManualStepContributor.java
@@ -1,5 +1,14 @@
 package ext.deployit.community.plugin.manualstep.contrib;
 
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.xebialabs.deployit.plugin.api.deployment.planning.Contributor;
@@ -7,20 +16,20 @@ import com.xebialabs.deployit.plugin.api.deployment.planning.DeploymentPlanningC
 import com.xebialabs.deployit.plugin.api.deployment.specification.Delta;
 import com.xebialabs.deployit.plugin.api.deployment.specification.Deltas;
 import com.xebialabs.deployit.plugin.api.deployment.specification.Operation;
+import com.xebialabs.deployit.plugin.api.udm.Container;
+import com.xebialabs.deployit.plugin.api.udm.Deployed;
 import com.xebialabs.deployit.plugin.api.udm.Environment;
+
 import ext.deployit.community.plugin.manualstep.ci.ContributorType;
 import ext.deployit.community.plugin.manualstep.ci.ManualStep;
 import ext.deployit.community.plugin.manualstep.ci.ManualSteps;
 import ext.deployit.community.plugin.manualstep.step.InstructionStep;
-
-import java.util.Map;
-
-import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Maps.newHashMap;
+import ext.deployit.community.plugin.manualstep.util.Util;
 
 public class ManualStepContributor {
 
-    @Contributor
+    @SuppressWarnings("rawtypes")
+	@Contributor
     public void triggerManualSteps(Deltas deltas, DeploymentPlanningContext ctx) {
         Operation operation = determineDeploymentOperation(deltas);
         Map<String, Object> commonVars = newHashMap();
@@ -35,6 +44,29 @@ public class ManualStepContributor {
             vars.put("step",manualStep);
             InstructionStep step = new InstructionStep(manualStep, vars);
             ctx.addStep(step);
+        }
+        
+        Map<String, Container> deployedContainers = newHashMap();
+        Set<Deployed> deployeds = ctx.getDeployedApplication().getDeployeds();
+        for (Deployed deployed : deployeds)	{
+        	Container container = deployed.getContainer();
+        	String containerIdentifier = Util.getContainerHostName(deployed.getContainer()) + "-" + container.getName();
+        	
+        	if (deployedContainers.get(containerIdentifier) == null)
+        		deployedContainers.put(containerIdentifier, container);
+        }
+        
+        Iterator<Entry<String, Container>> it = deployedContainers.entrySet().iterator();
+        while (it.hasNext())	{
+        	Map.Entry<String, Container> uniqueDeployedContainerPair = (Map.Entry<String, Container>) it.next();
+        	Container uniqueDeployedContainer = uniqueDeployedContainerPair.getValue();
+            Iterable<ManualStep> containerManualSteps = ManualSteps.getSteps(uniqueDeployedContainer, ContributorType.EVERY_SUBPLAN, operation);
+            for (ManualStep manualStep : containerManualSteps) {
+                HashMap<String,Object> vars = newHashMap(commonVars);
+                vars.put("step", manualStep);
+                InstructionStep step = new InstructionStep(manualStep, vars);
+                ctx.addStep(step);
+            }
         }
     }
 
--- deployit-udm-plugins/utility-plugins/manual-step-plugin/src/main/java/ext/deployit/community/plugin/manualstep/contrib/ManualStepProcessor.java
+++ deployit-udm-plugins/utility-plugins/manual-step-plugin/src/main/java/ext/deployit/community/plugin/manualstep/contrib/ManualStepProcessor.java
@@ -1,21 +1,28 @@
 package ext.deployit.community.plugin.manualstep.contrib;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import com.xebialabs.deployit.plugin.api.deployment.planning.PostPlanProcessor;
 import com.xebialabs.deployit.plugin.api.deployment.planning.PrePlanProcessor;
 import com.xebialabs.deployit.plugin.api.deployment.specification.DeltaSpecification;
 import com.xebialabs.deployit.plugin.api.flow.Step;
+import com.xebialabs.deployit.plugin.api.udm.Container;
+import com.xebialabs.deployit.plugin.api.udm.Deployed;
 import com.xebialabs.deployit.plugin.api.udm.Environment;
+
 import ext.deployit.community.plugin.manualstep.ci.ContributorType;
 import ext.deployit.community.plugin.manualstep.ci.ManualStep;
 import ext.deployit.community.plugin.manualstep.ci.ManualSteps;
 import ext.deployit.community.plugin.manualstep.step.InstructionStep;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Maps.newHashMap;
+import ext.deployit.community.plugin.manualstep.util.Util;
 
 public class ManualStepProcessor {
 
@@ -29,7 +36,8 @@ public class ManualStepProcessor {
         return triggerManualSteps(deltaSpec, ContributorType.ONCE_AT_THE_END);
     }
 
-    private List<Step> triggerManualSteps(DeltaSpecification deltaSpec, ContributorType contributorType) {
+    @SuppressWarnings("rawtypes")
+	private List<Step> triggerManualSteps(DeltaSpecification deltaSpec, ContributorType contributorType) {
         Map<String, Object> commonVars = newHashMap();
         commonVars.put("deltas", deltaSpec.getDeltas());
         commonVars.put("deployedApplication", deltaSpec.getDeployedApplication());
@@ -44,6 +52,29 @@ public class ManualStepProcessor {
             vars.put("step",manualStep);
             InstructionStep step = new InstructionStep(manualStep, vars);
             steps.add(step);
+        }
+        
+        Map<String, Container> deployedContainers = newHashMap();
+        Set<Deployed> deployeds = deltaSpec.getDeployedApplication().getDeployeds();
+        for (Deployed deployed : deployeds)	{
+        	Container container = deployed.getContainer();
+        	String containerIdentifier = Util.getContainerHostName(deployed.getContainer()) + "-" + container.getName();
+        	
+        	if (deployedContainers.get(containerIdentifier) == null)
+        		deployedContainers.put(containerIdentifier, container);
+        }
+        
+        Iterator<Entry<String, Container>> it = deployedContainers.entrySet().iterator();
+        while (it.hasNext())	{
+        	Map.Entry<String, Container> uniqueDeployedContainerPair = it.next();
+        	Container uniqueDeployedContainer = uniqueDeployedContainerPair.getValue();
+            Iterable<ManualStep> containerManualSteps = ManualSteps.getSteps(uniqueDeployedContainer, contributorType, deltaSpec.getOperation());
+            for (ManualStep manualStep : containerManualSteps) {
+                HashMap<String,Object> vars = newHashMap(commonVars);
+                vars.put("step", manualStep);
+                InstructionStep step = new InstructionStep(manualStep, vars);
+                steps.add(step);
+            }
         }
 
         return steps;
